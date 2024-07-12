5aee030cc515ae52ffc916e5038fa6b56d55dc8b
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-07-12 15:28:18 +0200

    Added operate function

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+let number1 
+let operator
+let number2
+
+function operate(number1, operator, number2) {
+    switch (operator) {
+        case '+':
+            add(number1, number2)
+            break
+        case '-':
+            subtract(number1, number2)
+            break
+        case 'x':
+            multiply(number1, number2)
+            break
+        case '/':
+            divide(number1, number2)
+            break
+    }
+}
+
 function add(a, b) {
 	return a + b
 };
